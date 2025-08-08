de722009056d27e3816875bf2c401711650017c4
octaviioViego <octavio.arroyo@estudiante.uacm.edu.mx>
2025-08-07 20:11:19 -0600

    Se agrego la función de suma.

--- ejercicios.c
+++ ejercicios.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
-int main(){
-    printf("Hola mundo\n");
+int suma(int numUno, int numDos){
+    return numUno + numDos;
+}
 
+int main(){
+    int numUno = 1;
+    int numeDos = 2;
+
+    int resultado = suma(numUno,numeDos);
+    printf("El resultado de la suma es: %d \n", resultado);
+    
     return 0;
 }
